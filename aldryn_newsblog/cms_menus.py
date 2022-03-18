a376617fb0332bce5fa0443452adcecbd6c56b4c
Jason2592 <formosa104@yahoo.com.tw>
2022-03-18 19:48:23 +0800

    Fix the covered commit (divio#514, divio#517, divio#519, divio#523)

--- aldryn_newsblog/cms_menus.py
+++ aldryn_newsblog/cms_menus.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 
+from django.core.exceptions import ImproperlyConfigured
 from django.urls import NoReverseMatch
 from django.utils.translation import get_language_from_request
 from django.utils.translation import ugettext_lazy as _
@@ -33,9 +34,12 @@ class NewsBlogMenu(CMSAttachMenu):
 
         if hasattr(self, 'instance') and self.instance:
             app = apphook_pool.get_apphook(self.instance.application_urls)
-            config = app.get_config(self.instance.application_namespace)
-            if config:
+            
+            try:
+                config = app.get_config(self.instance.application_namespace)
                 articles = articles.filter(app_config=config)
+            except AttributeError:
+                pass
 
         for article in articles:
             try:
